02b4d833fb704248ecba9b91f7a351379d1eced5
Dorians <dorianmarekwolny@wp.pl>
2026-06-22 23:53:36 +0200

    HitReaction

--- Assets/Dorian/Scripts/Enemy.cs
+++ Assets/Dorian/Scripts/Enemy.cs
@@ -3,22 +3,33 @@ using System;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
+    #region Events
     public event Action OnDamageTaken;
     public event Action<Vector3> OnDamageTakenWithPosition;
+    #endregion
 
+    #region Properties
     public int MaxHp => maxHp;
     public int CurrentHp => currentHp;
+    #endregion
 
+    #region Serialized Fields
     [SerializeField] private int maxHp = 100;
+    #endregion
 
+    #region Private Fields
     private int currentHp;
     private readonly int minHealth = 0;
+    #endregion
 
+    #region Unity Lifecycle
     private void Awake()
     {
         currentHp = maxHp;
     }
+    #endregion
 
+    #region Health Logic
     public void TakeDamage(int damage, Vector3 impactPosition)
     {
         OnDamageTaken?.Invoke();
@@ -44,4 +55,5 @@ public class Enemy : MonoBehaviour, IDamageable
     {
         Destroy(gameObject);
     }
+    #endregion
 }
